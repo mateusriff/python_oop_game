71c941085bd6c58a4e6a68e1e94fcace445fa95e
mateusriff <mateusnriff@gmail.com>
2024-03-20 13:43:49 -0300

    feat: export classes

--- src/classes/example_class.py
+++
@@ -1,9 +0,0 @@
-class ExampleClass:
-    def __init__(self): # constructor
-        self.value = 0
-
-    def get_value(self): # getter
-        return self.value
-
-    def set_value(self, val): # setter
-        self.value = val
