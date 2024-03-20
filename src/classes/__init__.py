ba60170436c114943425ccf3ebc42472394970a1
Mateus Riff <mateusnriff@gmail.com>
2024-03-20 13:52:00 -0300

    Add main game loop
    Squash and marge the following commits into `main`:
    * feat: export classes
    * feat: add main game loop

--- src/classes/__init__.py
+++ src/classes/__init__.py
@@ -1,10 +1,17 @@
 # Every time you create a new class, import it here.
 # Include the class name on the __all__ array to export it.
 
-from .example_class import ExampleClass  # importing class ExampleClass
 from .Game import Game
+from .Player import Player
+from .Bullet import Bullet
+from .Collectables import Collectable
+from .Text import Text, FadingText
 
 __all__ = [
-    'ExampleClass',  # exporting class ExampleClass
     'Game',
+    'Player',
+    'Bullet',
+    'Collectable',
+    'Text',
+    'FadingText',
 ]
